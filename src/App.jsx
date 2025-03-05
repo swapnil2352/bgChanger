cd6e9e395bfb33b8face5b6a5d5ab1c34640d20c
Swapnil Saxena <swapnilsaxena235@gmail.com>
2025-03-06 01:19:20 +0530

    test this app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p>My name is Arth Srivastava</p>
+    </>
+  )
+}
+
+export default App
